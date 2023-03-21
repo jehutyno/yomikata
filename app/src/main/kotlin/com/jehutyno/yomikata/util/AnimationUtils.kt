1fa0ac9215897d11261678b832149ed6e49b77fd
Lucas Dooms <lucas.dooms@hotmail.com>
2023-03-21 14:12:49 +0100

    Make animateSeekBar return the animation for later use and set startDelay to zero

--- app/src/main/kotlin/com/jehutyno/yomikata/util/AnimationUtils.kt
+++ app/src/main/kotlin/com/jehutyno/yomikata/util/AnimationUtils.kt
@@ -7,13 +7,14 @@ import android.widget.SeekBar
 /**
  * Created by valentin on 27/10/2016.
  */
-fun animateSeekBar(seekBar: SeekBar, from: Int, to: Int, max: Int) {
+fun animateSeekBar(seekBar: SeekBar, from: Int, to: Int, max: Int) : ObjectAnimator {
     seekBar.max = if (max < 10) max * 10 else max
     seekBar.progress = from
     val animation = ObjectAnimator.ofInt(seekBar, "progress", if (max < 10) to * 10 else to)
-    animation.startDelay = 300
+    animation.startDelay = 0
     animation.duration = 700
     animation.interpolator = DecelerateInterpolator()
     animation.start()
     seekBar.isEnabled = false
+    return animation
 }
