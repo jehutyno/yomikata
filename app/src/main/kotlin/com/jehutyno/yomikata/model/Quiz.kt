99ec7214a7355b5907f3818e642739c00ec954b5
Lucas Dooms <lucas.dooms@hotmail.com>
2023-04-02 13:26:16 +0200

    Change Quiz isSelected variable to Boolean

--- app/src/main/kotlin/com/jehutyno/yomikata/model/Quiz.kt
+++ app/src/main/kotlin/com/jehutyno/yomikata/model/Quiz.kt
@@ -5,7 +5,7 @@ import java.util.*
 /**
  * Created by valentin on 27/09/2016.
  */
-open class Quiz(val id: Long, var nameEn: String, var nameFr: String, val category: Int, var isSelected: Int) {
+open class Quiz(val id: Long, var nameEn: String, var nameFr: String, val category: Int, var isSelected: Boolean) {
 
     fun getName(): String {
         return if (Locale.getDefault().language == "fr")
