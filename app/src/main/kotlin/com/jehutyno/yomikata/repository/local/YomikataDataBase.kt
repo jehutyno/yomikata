648934edd9ecb3cdbe9cd86d0229bab9720a28d8
Lucas Dooms <lucas.dooms@hotmail.com>
2023-04-03 16:53:01 +0200

    Re-implement migration from 8 to 9 inside Room Database

--- app/src/main/kotlin/com/jehutyno/yomikata/repository/local/YomikataDataBase.kt
+++ app/src/main/kotlin/com/jehutyno/yomikata/repository/local/YomikataDataBase.kt
@@ -27,12 +27,16 @@ abstract class YomikataDataBase : RoomDatabase() {
                 synchronized(this) {
                     INSTANCE =
                         Room.databaseBuilder(context, YomikataDataBase::class.java, "yomikataz")
-                            .addMigrations(MIGRATION_8_9, MIGRATION_12_13)
+                            .allowMainThreadQueries()   // TODO: remove this after using coroutines/livedata
+                            .addMigrations(MIGRATION_8_9(context), MIGRATION_12_13)
                             .build()
                 }
             }
             return INSTANCE!!
         }
+
+        ///////// DEFINE MIGRATIONS /////////
+        // do not use values or constants that may be changed externally
 
         // migrate from anko sqlite to room
         private val MIGRATION_12_13 = object : Migration(12, 13) {
@@ -41,48 +45,67 @@ abstract class YomikataDataBase : RoomDatabase() {
             }
         }
 
-        private val MIGRATION_8_9 = object : Migration(8, 9) {
+        @Suppress("ClassName")
+        private class MIGRATION_8_9(val context: Context) : Migration(8, 9) {
+            // STEPS
+            // 1. Add sentences table (did not exist before version 9)
+            // 2. Alter the words table: new column = sentenceId (+ redefine types??) (all others preserved)
+            // 3. Add all of the new sentences
             override fun migrate(database: SupportSQLiteDatabase) {
-                val query0 = "ALTER TABLE ${SQLiteTables.WORDS.tableName} RENAME TO 'OLD_${SQLiteTables.WORDS.tableName}'"
-
-                val query1 = "CREATE TABLE words (\n" +
-                        "    ${SQLiteWord.ID.column}           INTEGER      PRIMARY KEY AUTOINCREMENT,\n" +
-                        "    ${SQLiteWord.JAPANESE.column}      TEXT         NOT NULL,\n" +
-                        "    ${SQLiteWord.ENGLISH.column}       TEXT         NOT NULL,\n" +
-                        "    ${SQLiteWord.FRENCH.column}        TEXT         NOT NULL,\n" +
-                        "    ${SQLiteWord.READING.column}       TEXT         NOT NULL,\n" +
-                        "    ${SQLiteWord.LEVEL.column}         INTEGER,\n" +
-                        "    ${SQLiteWord.COUNT_TRY.column}     INTEGER,\n" +
-                        "    ${SQLiteWord.COUNT_SUCCESS.column} INTEGER,\n" +
-                        "    ${SQLiteWord.COUNT_FAIL.column}    INTEGER,\n" +
-                        "    ${SQLiteWord.IS_KANA.column}       BOOLEAN,\n" +
-                        "    ${SQLiteWord.REPETITION.column}    INTEGER (-1),\n" +
-                        "    ${SQLiteWord.POINTS.column}        INTEGER,\n" +
-                        "    ${SQLiteWord.BASE_CATEGORY.column} INTEGER,\n" +
-                        "    ${SQLiteWord.IS_SELECTED.column}    INTEGER      DEFAULT (0),\n" +
-                        "    ${SQLiteWord.SENTENCE_ID.column}   INTEGER      DEFAULT ( -1) \n" +
+                // STEP 1
+                val queryCreateSentencesTable = "CREATE TABLE sentences (\n" +
+                        "   _id         INTEGER     PRIMARY KEY,\n" +
+                        "   jap         TEXT,\n" +
+                        "   en          TEXT,\n" +
+                        "   fr          TEXT,\n" +
+                        "   level       INTEGER\n" +
                         ");"
 
-                val query2 = "INSERT INTO ${SQLiteTables.WORDS.tableName} " +
-                        "(${SQLiteWord.ID.column}, ${SQLiteWord.JAPANESE.column}, ${SQLiteWord.ENGLISH.column}, ${SQLiteWord.FRENCH.column}," +
-                        "${SQLiteWord.READING.column}, ${SQLiteWord.LEVEL.column}, ${SQLiteWord.COUNT_TRY.column}, " +
-                        "${SQLiteWord.COUNT_SUCCESS.column}, ${SQLiteWord.COUNT_FAIL.column}, ${SQLiteWord.IS_KANA.column}, " +
-                        "${SQLiteWord.REPETITION.column}, ${SQLiteWord.POINTS.column}, ${SQLiteWord.BASE_CATEGORY.column}, " +
-                        "${SQLiteWord.IS_SELECTED.column}) " +
-                        "SELECT ${SQLiteWord.ID.column}, ${SQLiteWord.JAPANESE.column}, ${SQLiteWord.ENGLISH.column}, ${SQLiteWord.FRENCH.column}, " +
-                        "${SQLiteWord.READING.column}, ${SQLiteWord.LEVEL.column}, ${SQLiteWord.COUNT_TRY.column}, " +
-                        "${SQLiteWord.COUNT_SUCCESS.column}, ${SQLiteWord.COUNT_FAIL.column}, ${SQLiteWord.IS_KANA.column}, " +
-                        "${SQLiteWord.REPETITION.column}, ${SQLiteWord.POINTS.column}, ${SQLiteWord.BASE_CATEGORY.column}, " +
-                        "${SQLiteWord.IS_SELECTED.column} FROM OLD_${SQLiteTables.WORDS.tableName}"
+                database.execSQL(queryCreateSentencesTable)
 
-                val query3 = "DROP TABLE OLD_${SQLiteTables.WORDS.tableName}"
+                // STEP 2
+                val queryRenameOldTable = "ALTER TABLE words RENAME TO OLD_words"
+
+                val queryCreateNewTable = "CREATE TABLE words (\n" +
+                        "    _id           INTEGER      PRIMARY KEY AUTOINCREMENT,\n" +
+                        "    japanese      TEXT         NOT NULL,\n" +
+                        "    english       TEXT         NOT NULL,\n" +
+                        "    french        TEXT         NOT NULL,\n" +
+                        "    reading       TEXT         NOT NULL,\n" +
+                        "    level         INTEGER,\n" +
+                        "    count_try     INTEGER,\n" +
+                        "    count_success INTEGER,\n" +
+                        "    count_fail    INTEGER,\n" +
+                        "    is_kana       BOOLEAN,\n" +
+                        "    repetition    INTEGER (-1),\n" +
+                        "    points        INTEGER,\n" +
+                        "    base_category INTEGER,\n" +
+                        "    isSelected    INTEGER      DEFAULT (0),\n" +
+                        "    sentence_id   INTEGER      DEFAULT (-1) \n" +
+                        ");"
+
+                val queryInsertOldIntoNew = "INSERT INTO words " +
+                        "(_id, japanese, english, french, reading, level, count_try, count_success, " +
+                        "       count_fail, is_kana, repetition, points, base_category, isSelected) " +
+                        "SELECT _id, japanese, english, french, reading, level, count_try, count_success, " +
+                        "       count_fail, is_kana, repetition, points, base_category, isSelected " +
+                        "FROM OLD_words"
+
+                val queryDropOldTable = "DROP TABLE OLD_words"
 
                 database.use {
-                    it.execSQL(query0)
-                    it.execSQL(query1)
-                    it.execSQL(query2)
-                    it.execSQL(query3)
+                    it.execSQL(queryRenameOldTable)
+                    it.execSQL(queryCreateNewTable)
+                    it.execSQL(queryInsertOldIntoNew)
+                    it.execSQL(queryDropOldTable)
                 }
+
+                // STEP 3
+                val queryInsertAllSentences = context.assets.open("InsertSentencesVersion9.sql")
+                            .bufferedReader().use {
+                                it.readText()
+                            }
+                database.execSQL(queryInsertAllSentences)
             }
         }
 
