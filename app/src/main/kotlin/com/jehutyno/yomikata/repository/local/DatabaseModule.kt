8f55921446218f6ecee76ebf2c01af6f99c08bb6
Lucas Dooms <lucas.dooms@hotmail.com>
2023-04-06 15:27:52 +0200

    Change kodein singleton to provider for database/dao instances

--- app/src/main/kotlin/com/jehutyno/yomikata/repository/local/DatabaseModule.kt
+++ app/src/main/kotlin/com/jehutyno/yomikata/repository/local/DatabaseModule.kt
@@ -1,17 +1,14 @@
 package com.jehutyno.yomikata.repository.local
 
 import android.content.Context
-import org.kodein.di.DI
-import org.kodein.di.bind
-import org.kodein.di.instance
-import org.kodein.di.singleton
+import org.kodein.di.*
 
 
 fun databaseModule(context: Context) = DI.Module("databaseModule") {
     bind<YomikataDataBase>() with instance(YomikataDataBase.getDatabase(context))
-    bind<QuizDao>() with singleton { instance<YomikataDataBase>().quizDao() }
-    bind<WordDao>() with singleton { instance<YomikataDataBase>().wordDao() }
-    bind<StatsDao>() with singleton { instance<YomikataDataBase>().statsDao() }
-    bind<KanjiSoloDao>() with singleton { instance<YomikataDataBase>().kanjiSoloDao() }
-    bind<SentenceDao>() with singleton { instance<YomikataDataBase>().sentenceDao() }
+    bind<QuizDao>() with provider { instance<YomikataDataBase>().quizDao() }
+    bind<WordDao>() with provider { instance<YomikataDataBase>().wordDao() }
+    bind<StatsDao>() with provider { instance<YomikataDataBase>().statsDao() }
+    bind<KanjiSoloDao>() with provider { instance<YomikataDataBase>().kanjiSoloDao() }
+    bind<SentenceDao>() with provider { instance<YomikataDataBase>().sentenceDao() }
 }
