a255983363cec14b0d133b961b8ab53e879bb20b
Lucas Dooms <lucas.dooms@hotmail.com>
2023-05-11 23:57:36 +0200

    Create source directory for base presenters

--- app/src/main/kotlin/com/jehutyno/yomikata/presenters/source/WordCountPresenter.kt
+++ app/src/main/kotlin/com/jehutyno/yomikata/presenters/source/WordCountPresenter.kt
@@ -1,9 +1,10 @@
-package com.jehutyno.yomikata.presenters
+package com.jehutyno.yomikata.presenters.source
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asFlow
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.distinctUntilChanged
+import com.jehutyno.yomikata.presenters.WordCountInterface
 import com.jehutyno.yomikata.repository.QuizRepository
 import com.jehutyno.yomikata.util.Level
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -13,7 +14,8 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 
 
-class WordCountPresenter(quizRepository: QuizRepository, quizIdsFlow: Flow<LongArray>): WordCountInterface {
+class WordCountPresenter(quizRepository: QuizRepository, quizIdsFlow: Flow<LongArray>):
+    WordCountInterface {
 
     // If quizIds does not change, use this constructor
     constructor(quizRepository: QuizRepository, quizIds: LongArray) : this(quizRepository, flow{emit(quizIds)})
